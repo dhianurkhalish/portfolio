d99c35087deb1222b15fd02da60fe9e8d02abebe
dhianakmakannn <dhianurkhalish@gmail.com>
2026-08-28 23:25:34 +0800

    academic.html logbook.html main.html main.js images/ep1.png images/ep2.png images/ep3.png images/ep4.png images/ep5.png images/ep6.png images/gallery1.jpg images/gallery2.jpg images/gallery3.jpg images/gallery5.jpg images/gallery6.jpg images/ict (1).png images/ict (2).png images/ict (3).png images/ict (4).png images/ict (5).png

--- main.js
+++ main.js
@@ -7,3 +7,34 @@ function toggleProjects(event) {
     subnav.classList.toggle("show");
     arrow.classList.toggle("rotate");
 }
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+let currentSlide = 0;
+
+function showSlide(index) {
+
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    dots.forEach(dot => {
+        dot.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+    dots[index].classList.add("active");
+}
+
+function nextSlide() {
+
+    currentSlide++;
+
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000);
